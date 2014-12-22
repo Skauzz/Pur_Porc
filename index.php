0534e20432380e98e0385b092be6bb1cd259d398
Bernier Kévin <bernier.kevin59@gmail.com>
2014-12-22 10:14:07 +0100

    Html Template
    Home Template structure

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="fr">
+	<head>
+	  <meta charset="utf-8">
+	  <title>Pure Porc Home Page</title>
+	  <link rel="stylesheet" href="css/style.css">
+	  <link rel="stylesheet" href="css/bootstrap.css">
+	  <link rel="stylesheet" href="css/bootstrap-theme.css">
+	   <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+	  <script src="js/script.js"></script>
+	  <script src="js/bootstrap.js"></script>
+	  <script src="js/npm.js"></script>
+	</head>
+		<body>
+			<?php
+				require_once('header.html');
+				require_once('menu.html');
+				require_once('content.html');
+				require_once('footer.html');
+			?>
+		</body>
+</html>
